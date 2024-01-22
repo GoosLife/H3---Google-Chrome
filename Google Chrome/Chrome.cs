6f51f49c6a1f1569aa0f7ef836323f33966d7a54
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2024-01-22 11:03:22 +0100

    Add project files.

---
+++ Google Chrome/Chrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Google_Chrome
+{
+    internal class Chrome
+    {
+        private HttpClient client;
+
+        public Chrome()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<string> GetWebsite(string uri)
+        {
+            string? website = null;
+
+            if (uri == "" || uri == null)
+            {
+                throw new ArgumentException("URI was empty");
+            }
+
+            uri = ValidateURI(uri);
+
+            try
+            {
+                website = await client.GetStringAsync(uri);
+            }
+            catch (HttpRequestException e)
+            {
+                return e.Message;
+            }
+
+            return website;
+        }
+
+        private string ValidateURI(string uri)
+        {
+            if (!uri.StartsWith("http://") || uri.StartsWith("https://"))
+            {
+                uri = "http://" + uri;
+            }
+
+            return uri;
+        }
+    }
+}
---
+++ Google Chrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+
+namespace Google_Chrome
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Chrome chrome = new Chrome();
+
+                bool running = true;
+                string? uri = "";
+
+                while (running)
+                {
+                    while (uri == "")
+                    {
+                        Console.Write("Adress: ");
+                        uri = Console.ReadLine();
+
+                        if (uri == "exit")
+                        {
+                            running = false;
+                            return;
+                        }
+                    }
+
+                    Console.WriteLine(await chrome.GetWebsite(uri!)!);
+
+                    uri = "";
+
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
